bbea7a221c29123e6729c018ed6e06dc3ae4e7b3
Prabha Pothen <prabha.pothen@gmail.com>
2021-05-25 15:53:38 +0200

    corrections made

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -8,18 +8,22 @@ class IslandsController < ApplicationController
   end
 
   def edit
-    @island = Island.find(params[:id])
+
   end
 
   def update
-    @island = Island.find(params[:id])
     @island.update(params[:island])
     redirect_to_island_path(@island)
   end
-end
+
 
 private
 
-def islands_params
-  params.require(:island).permit(:name, :address, :description, :daily_rate, :availability, :user_id)
+  def islands_params
+    params.require(:island).permit(:name, :address, :description, :daily_rate, :availability, :user_id)
+  end
+
+  def set_island
+  @island = Island.find(params[:id])
+  end
 end
