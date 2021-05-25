c90feb80fcbc77a355db5f0d41f1a6de84144648
Vedran Hamovic <vedranh0@gmail.com>
2021-05-25 17:13:17 +0200

    added a dot

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-puts "Cleaning database..."
+puts "Cleaning database...."
 Island.destroy_all
 
 puts "DB cleaned"
