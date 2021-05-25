94b2ed81eb651884f65555628d104cccd2ad9f55
Prabha Pothen <prabha.pothen@gmail.com>
2021-05-25 12:44:16 +0200

    added model ISLAND

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_103910) do
+ActiveRecord::Schema.define(version: 2021_05_25_104224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "islands", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.integer "daily_rate"
+    t.boolean "availability"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_islands_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_05_25_103910) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "islands", "users"
 end
