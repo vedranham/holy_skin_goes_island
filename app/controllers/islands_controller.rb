d5c511e2463361ae986707fa3bd2398ddbac4ec4
Prabha Pothen <prabha.pothen@gmail.com>
2021-05-25 15:34:46 +0200

    created edit & update

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -6,4 +6,20 @@ class IslandsController < ApplicationController
   def show
     @island = Island.find(params[:id])
   end
+
+  def edit
+    @island = Island.find(params[:id])
+  end
+
+  def update
+    @island = Island.find(params[:id])
+    @island.update(params[:island])
+    redirect_to_island_path(@island)
+  end
+end
+
+private
+
+def islands_params
+  params.require(:island).permit(:name, :address, :description, :daily_rate, :availability, :user_id)
 end
