91941ddde0aec2b826531f0a295155326dc9a616
Vedran Hamovic <vedranh0@gmail.com>
2021-05-25 15:52:13 +0200

    create created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # index
   get "/", to: 'islands#index'
+
+  get 'islands/new', to: 'islands#new'
  # show
- get 'islands/:id', to: 'island#show', as: :island
+  get 'islands/:id', to: 'islands#show', as: :island
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ # new
+  # create
+  post 'islands', to: 'islands#create'
+
 end
