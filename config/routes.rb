349510ca2801d074649bdbca7dd3df00d3313e95
Vedran Hamovic <vedranh0@gmail.com>
2021-05-27 11:46:54 +0200

    routes sorted

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # index
-  root to: "pages#home"
-  get "/islands", to: 'islands#index'
-
-  get '/islands/new', to: 'islands#new'
-  post '/islands', to: 'islands#create'
- # show
- get '/islands/:id', to: 'islands#show', as: :island
- #edit
-  get '/islands/:id/edit', to: 'islands#edit'
- #update
- patch '/islands/:id', to: 'islands#update'
- #destroy
- delete '/islands/:id', to: 'islands#destroy'
+  root to: "islands#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :islands, except: :index
 end
