a93dbf68e7c572386a75169cbd9b072471f00193
Jishuo Ma <jishuoma@gmail.com>
2021-05-27 18:18:42 +0200

    island create fix

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,5 +1,7 @@
 class IslandsController < ApplicationController
   before_action :set_island, only: [:show]
+  before_action :set_user, only: [:create]
+
   def index
     @islands = Island.all
   end
@@ -13,9 +15,13 @@ class IslandsController < ApplicationController
   end
 
   def create
-    island = Island.new(island_params)
-    island.save
-    redirect_to island_path(island)
+    @island = Island.new(island_params)
+    @island.user_id = @user.id
+    if @island.save
+      redirect_to island_path(@island)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -33,10 +39,14 @@ class IslandsController < ApplicationController
 private
 
   def island_params
-    params.require(:island).permit(:name, :address, :description, :daily_rate, :availability, :user_id)
+    params.require(:island).permit(:name, :address, :description, :daily_rate, :availability)
   end
 
   def set_island
     @island = Island.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
   end
 end
